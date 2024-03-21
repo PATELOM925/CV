284aaccdace1884036575c4cf503fc39506aa296
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2024-03-21 16:26:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,22 +36,22 @@ SOCIAL_MEDIA = {
 PROJECTS = {
     'SQl AI - Access Database In Your Language \n': {
         'url': 'https://github.com/PATELOM925/SQL-AI',
-        'description': "- Users can upload SQL databases \n"
-                       "- Implements advanced natural language prompts \n"
-                       "- Transforms input into precise SQL queries"
+        'description': "Users can upload SQL databases \n",
+                       "Implements advanced natural language prompts \n",
+                       "Transforms input into precise SQL queries"
     },
      'ChatPDF AI - Talk With Your PDFs \n': {
         'url': 'https://chatpdf-ai-om-m-patel.streamlit.app/',
-        'description': "- Leverages advanced NLP techniques \n"
-                       "- Integration with Streamlit Cloud for streamlined PDF analysis \n"
-                       "- Achieves an impressive 89% accuracy rate \n"
+        'description': "- Leverages advanced NLP techniques \n",
+                       "- Integration with Streamlit Cloud for streamlined PDF analysis \n",
+                       "- Achieves an impressive 89% accuracy rate \n",
                        "- Empowers users in making informed decisions faster"
     },
     'Autograder - Precision In Every Grade \n': {
         'url': 'https://github.com/PATELOM925/Automatic-Paper-Checker',
-        'description': "- Automated grading system for teachers and professors \n"
-                       "- Integration of BERT-Uncased for NLP-based grading \n"
-                       "- Utilizes Spacy & PyTesseract for image processing \n"
+        'description': "- Automated grading system for teachers and professors \n",
+                       "- Integration of BERT-Uncased for NLP-based grading \n",
+                       "- Utilizes Spacy & PyTesseract for image processing \n",
                        "- Ensures accuracy in assessing diverse exam formats"
     },
     'Meme App': {
