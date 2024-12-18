c6c8cd57f0344620b1eae733734350c3c0e4a496
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2024-12-18 22:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,22 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+st.markdown(
+    """
+    <script>
+    document.addEventListener('scroll', function () {
+        const body = document.querySelector('body');
+        if (window.scrollY > 50) { // Adjust threshold as needed
+            body.classList.add('scrolled');
+        } else {
+            body.classList.remove('scrolled');
+        }
+    });
+    </script>
+    """,
+    unsafe_allow_html=True
+)
+
 # st.markdown(
 #     """
 #     <style>
