ba732a34cccaad48c02948238c0aea7a280551b1
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2025-07-31 19:17:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import requests 
-
+ 
 # ----------------------------
 # PAGE CONFIGURATION & PATHS 
 # ---------------------------- 
